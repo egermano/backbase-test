7434ef31a221beefa79e8fdd7b6409f7b4ed1509
Bruno Germano <bruno.germano@gmail.com>
2015-03-15 19:43:37 +0000

    get items from pipes, filter and sort them

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,42 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+(function($){
+  var App = {};
+
+  App.totalItems = 10;
+
+  App.init = function(){
+    this.loadData(this.totalItems).then(function(data){
+      data = App.sortItems(data);
+      console.log(data);
+    });
+  };
+
+  App.loadData = function(total){
+    return $.get('http://pipes.yahoo.com/pipes/pipe.run?_id=DqsF_ZG72xGLbes9l7okhQ&_render=json').then(function(data){
+      return data.value.items.splice(0,total);
+    });
+  };
+
+  App.sortItems = function(items){
+    // create attribute with locale date and time
+    for (var i = items.length - 1; i >= 0; i--) {
+      var newDate = new Date(items[i].pubDate);
+      items[i].localeDate = newDate.toLocaleDateString();
+      items[i].localeTime = newDate.toLocaleTimeString();
+    }
+
+    return items.sort(function(a, b){
+      var dateA = new Date(a.pubDate),
+          dateB = new Date(b.pubDate);
+
+      return dateA < dateB;
+    });
+  };
+
+  App.buildItem = function(){
+
+  };
+
+  App.init();
+})(jQuery);
